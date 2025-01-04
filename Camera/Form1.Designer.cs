ad9d035951a3a089557249b94fd78f8cdcbbb3e0
myddy type <sserduk56@gmail.com>
2025-01-04 11:42:07 +0200

    First Commit

---
+++ Camera/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.пускToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(34, 408);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(527, 30);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(567, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("menuStrip1.BackgroundImage")));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.пускToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // пускToolStripMenuItem
+            // 
+            this.пускToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.пускToolStripMenuItem.Name = "пускToolStripMenuItem";
+            this.пускToolStripMenuItem.Size = new System.Drawing.Size(156, 20);
+            this.пускToolStripMenuItem.Text = "Информация о продукте";
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(603, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 30);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(634, 271);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem пускToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Camera/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using DirectShowLib;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Camera
+{//taskkill /F /IM ffmpeg.exe для закрытия через cmd процессов 
+ //tasklist | findstr ffmpeg для просмотра того,на что распостранён ffmpage 
+
+    public partial class Form1 : Form
+    {
+        public string pathFFmpeg = @"C:\Install\peg\ffmpeg.exe"; 
+        public string Device = string.Empty;
+        public DemoRecord record;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int count = 0;
+            var videoDevices = new List<DsDevice>(DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice));
+            foreach (var i in videoDevices)
+            {
+                listBox1.Items.Add($"{count + 1}: {i.Name}");
+                count++;
+            }
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            listBox1.SelectedIndex = -1;
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Device = listBox1.SelectedItem.ToString().Split(':')[1].Trim();
+            MessageBox.Show($"Используется {Device.Trim().ToString()}");
+        }
+        public class DemoRecord
+        {
+            public Process ffmpegProcess;
+            private CancellationTokenSource cancellationTokenSource;
+            public DemoRecord() { }
+            public async Task StartRecordAsync(string device, string pathFfmpeg, PictureBox pictureBox)
+            {
+                if (string.IsNullOrEmpty(device))
+                {
+                    MessageBox.Show("Выберите устройство!");
+                    return;
+                }
+
+                try
+                {
+                    ffmpegProcess = new Process();
+                    cancellationTokenSource = new CancellationTokenSource();
+                    ffmpegProcess.StartInfo.FileName = pathFfmpeg;
+                    string outputFile = $"C:\\Users\\user\\Videos\\video_{DateTime.Now:yyyyMMdd_HHmmss}.avi";
+                    ffmpegProcess.StartInfo.Arguments = $"-f dshow -i video=\"{device}\" -vcodec libx264 -preset ultrafast -t 00:10:00 \"{outputFile}\" -vf fps=100 -f image2pipe -vcodec png pipe:1";
+
+                    ffmpegProcess.StartInfo.UseShellExecute = false;
+                    ffmpegProcess.StartInfo.RedirectStandardOutput = true;
+                    ffmpegProcess.StartInfo.RedirectStandardError = true;
+                    ffmpegProcess.StartInfo.CreateNoWindow = true;
+
+                    ffmpegProcess.ErrorDataReceived += (sender, e) =>
+                    {
+                        if (e.Data != null)
+                        {
+                            Console.WriteLine($"stderr: {e.Data}");
+                        }
+                    };
+
+                    ffmpegProcess.Start();
+
+                    Stream outputStream = ffmpegProcess.StandardOutput.BaseStream;
+
+                    // Чтение данных изображения в отдельном потоке.
+                    await Task.WhenAny(
+                        ProcessFramesAsync(outputStream, pictureBox, cancellationTokenSource.Token),
+                        Task.Run(() => ffmpegProcess.WaitForExit())
+                    );
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show($"Ошибка: {error.Message}");
+                }
+            
+        }
+            public void StopRecord()
+            {
+                if (ffmpegProcess != null && !ffmpegProcess.HasExited)
+                {
+                    ffmpegProcess.Kill();
+                    ffmpegProcess = null;
+                    MessageBox.Show("Конец записи!");
+                }
+                else
+                {
+                    MessageBox.Show("Запись не запущена!");
+                }
+            }
+            private async Task ProcessFramesAsync(Stream stream, PictureBox pictureBox, CancellationToken cancellationToken)
+            {
+                byte[] buffer = new byte[1024 * 1024]; 
+                int bytesRead = 0;
+
+                try
+                {
+                    while (!cancellationToken.IsCancellationRequested)
+                    {
+                        bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+                        if (bytesRead <= 0)
+                            break;
+
+                        
+                        using (MemoryStream ms = new MemoryStream(buffer, 0, bytesRead))
+                        {
+                            pictureBox.Invoke((MethodInvoker)(() =>
+                            {
+                                pictureBox.Image = new Bitmap(ms);
+                            }));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка обработки кадров: {ex.Message}");
+                }
+            }
+        }
+
+            private async void button1_Click(object sender, EventArgs e)
+        {
+            record = new DemoRecord();
+            await record.StartRecordAsync(Device, pathFFmpeg, pictureBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (record != null)
+            {
+                record.StopRecord();
+            }
+            else
+            {
+                MessageBox.Show("Запись не была начата!");
+            }
+        }
+    }
+}
